a12bb84579e58431bcc9e23933a150f9b3062926
Ziru Zou <jiyucoding@yahoo.com>
2023-02-17 18:36:11 -0500

    python data types example

---
+++ app.py
@@ -0,0 +1,33 @@
+dog_name = "Lucy"
+print(f"Say hello to my dog {dog_name}")
+
+
+# display price with two decimal places - ".2f"
+price_1 = 3.5
+print(f"item 1 costs ${price_1: .2f}")
+
+int_number = int("1")
+print(int_number)
+
+float_number = float(4 / 3)
+print(float_number)
+
+list_abc = ["a", "b", "c"]
+print(list_abc[0])
+
+tuple_example = [(1, 2, 3), ("a", "b", "c"), (1,)]
+print(tuple_example[2])
+
+set_1 = set((1, 2, 3, 4))
+print(set_1)  # {1, 2, 3, 4}
+
+set_2 = set([1, 2, 2, 3, 4, "a", "a"])
+print(set_2)    # {1, 2, 3, 4, 'a'}
+print(dir(set()))
+
+my_dict = { "key1": "value1", "key2": "value2" 
+print(my_dict["key2"])
+print(my_dict.get("key1"))
+dict(x = 1, y = 2)
+# => {'x': 1, 'y': 2}
+
